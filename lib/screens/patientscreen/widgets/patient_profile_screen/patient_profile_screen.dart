200b933351b2484eeb9050f143c5796c07f53b49
AquariumFish2 <you@example.com>
2021-12-30 16:05:20 +0200

    ui changes

--- lib/screens/patientscreen/widgets/patient_profile_screen/patient_profile_screen.dart
+++ lib/screens/patientscreen/widgets/patient_profile_screen/patient_profile_screen.dart
@@ -4,9 +4,9 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:sample/provider/account.dart';
 import 'package:sample/provider/patient.dart';
-import 'package:sample/provider/patients.dart';
-import 'package:sample/screens/patientscreen/widgets/patient_list.dart';
+
 import 'package:sample/screens/patientscreen/widgets/patient_profile_screen/widgets/patient_profile_listtile.dart';
+import 'package:sample/screens/widgets/custom_text_field.dart';
 
 //need to clean
 class PatientProfileScreen extends StatelessWidget {
@@ -14,10 +14,12 @@ class PatientProfileScreen extends StatelessWidget {
     Key? key,
   }) : super(key: key);
   TextEditingController dialogController = TextEditingController();
+  final Key dialogKey = const Key('dialogKey');
   @override
   Widget build(BuildContext context) {
     final patient = Provider.of<Patient>(context);
     final account = Provider.of<Account>(context);
+    bool sameUser = patient.volId == account.id;
     return Scaffold(
       backgroundColor: Colors.green,
       appBar: AppBar(
@@ -44,36 +46,47 @@ class PatientProfileScreen extends StatelessWidget {
           PatientProfieListTile(
             title: 'الاسم :',
             trailing: patient.name,
+            sameUser: sameUser,
           ),
           PatientProfieListTile(
-              title: 'اسم المتابع', trailing: patient.volName),
+            title: 'اسم المتابع',
+            trailing: patient.volName,
+            sameUser: sameUser,
+          ),
           PatientProfieListTile(
             title: 'الرقم القومي :',
             trailing: patient.nationalId,
+            sameUser: sameUser,
           ),
           PatientProfieListTile(
             title: 'المركز :',
             trailing: patient.state,
+            sameUser: sameUser,
           ),
           PatientProfieListTile(
             title: 'العنوان :',
             trailing: patient.address,
+            sameUser: sameUser,
           ),
           PatientProfieListTile(
             title: 'رقم المحمول :',
             trailing: patient.phone,
+            sameUser: sameUser,
           ),
           PatientProfieListTile(
             title: 'السورس :',
             trailing: patient.source,
+            sameUser: sameUser,
           ),
           PatientProfieListTile(
             title: 'اسم الطبيب المتابع :',
             trailing: patient.doctor,
+            sameUser: sameUser,
           ),
           PatientProfieListTile(
             title: 'نوع المرض :',
             trailing: patient.illnessType,
+            sameUser: sameUser,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -93,34 +106,58 @@ class PatientProfileScreen extends StatelessWidget {
                       showDialog(
                           context: context,
                           builder: (ctx) => Dialog(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text('أدخل أخر ما وصلته مع الحالة :-'),
-                                    Text(patient.name as String),
-                                    TextField(
-                                      controller: dialogController,
-                                    ),
-                                    TextButton(
-                                        onPressed: () async {
-                                          await FirebaseDatabase.instance
-                                              .ref()
-                                              .child('patients')
-                                              .child(account.team as String)
-                                              .child(
-                                                  patient.nationalId as String)
-                                              .child('latests')
-                                              .child(
-                                                  '${(DateTime.now().second)}:${(DateTime.now().millisecond)}')
-                                              .set({
-                                            'title': dialogController.text,
-                                            'date': DateTime.now().toString(),
-                                          });
-                                          Navigator.of(ctx).pop();
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text('save')),
-                                  ],
+                                child: Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'أدخل أخر ما وصلته مع الحالة :-',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.green,
+                                        ),
+                                      ),
+                                      Text(
+                                        patient.name as String,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.green,
+                                        ),
+                                      ),
+                                      AddPatientTextField(
+                                        controller: dialogController,
+                                        label: 'ادخل الاضافه',
+                                        multiline: false,
+                                        save: (v) {},
+                                        validate: (v) {},
+                                        tKey: dialogKey,
+                                      ),
+                                      TextButton(
+                                          onPressed: () async {
+                                            await FirebaseDatabase.instance
+                                                .ref()
+                                                .child('patients')
+                                                .child(account.team as String)
+                                                .child(patient.nationalId
+                                                    as String)
+                                                .child('latests')
+                                                .child(
+                                                    '${(DateTime.now().second)}:${(DateTime.now().millisecond)}')
+                                                .set({
+                                              'title': dialogController.text,
+                                              'date': DateTime.now().toString(),
+                                            });
+                                            Navigator.of(ctx).pop();
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text('save')),
+                                    ],
+                                  ),
                                 ),
                               ));
                     },
--- lib/screens/patientscreen/widgets/patient_profile_screen/widgets/patient_profile_listtile.dart
+++ lib/screens/patientscreen/widgets/patient_profile_screen/widgets/patient_profile_listtile.dart
@@ -2,20 +2,26 @@ import 'package:flutter/material.dart';
 
 class PatientProfieListTile extends StatelessWidget {
   const PatientProfieListTile(
-      {Key? key, required this.title, required this.trailing})
+      {Key? key,
+      required this.title,
+      required this.trailing,
+      required this.sameUser})
       : super(key: key);
   final String? title;
   final String? trailing;
+  final bool sameUser;
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: IconButton(
-        onPressed: () {},
-        icon: Icon(
-          Icons.edit,
-          color: Colors.white,
-        ),
-      ),
+      leading: (sameUser)
+          ? IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.edit,
+                color: Colors.white,
+              ),
+            )
+          : null,
       title: Text(
         (title != null) ? title as String : '',
         style: Theme.of(context).textTheme.headline2,
