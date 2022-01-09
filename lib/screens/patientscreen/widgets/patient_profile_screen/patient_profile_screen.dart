b0db4424e52f9d95a686eac6319ff30755365ac8
abdelrahmanAbdelkader <abdominos1000@gmail.com>
2022-01-09 19:15:33 +0200

    forget this changes to push

--- lib/screens/patientscreen/widgets/patient_profile_screen/patient_profile_screen.dart
+++ lib/screens/patientscreen/widgets/patient_profile_screen/patient_profile_screen.dart
@@ -8,7 +8,9 @@ import 'package:sample/provider/patient.dart';
 import 'package:sample/provider/patients.dart';
 import 'package:sample/screens/patientscreen/add_patient_screen/widgets/patient_screen_doctors_dropdownbutton.dart';
 import 'package:sample/screens/patientscreen/add_patient_screen/widgets/state_dropdownbutton.dart';
+import 'package:sample/screens/patientscreen/widgets/patient_profile_screen/widgets/dropDownDialogProfPatientScreen.dart';
 import 'package:sample/screens/patientscreen/widgets/patient_profile_screen/widgets/patient_profile_listtile.dart';
+import 'package:sample/screens/patientscreen/widgets/patient_profile_screen/widgets/textFieldDialogProfPatientScreen.dart';
 import 'package:sample/screens/widgets/custom_text_field.dart';
 
 //need to clean
@@ -508,138 +510,4 @@ class PatientProfileScreen extends StatelessWidget {
       ),
     );
   }
-}
-
-class TextFieldDialog extends Dialog {
-  TextFieldDialog({
-    required String typeChanges,
-    required String currentValue,
-    required TextEditingController controller,
-    required String keyOfDataBase,
-  }) : super(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(typeChanges),
-              Text(currentValue),
-              TextField(
-                controller: controller,
-              ),
-              Builder(builder: (context) {
-                return TextButton(
-                    onPressed: () async {
-                      final account =
-                          Provider.of<Account>(context, listen: false);
-                      final patient =
-                          Provider.of<Patient>(context, listen: false);
-                      if (keyOfDataBase != 'nationId')
-                        await FirebaseDatabase.instance
-                            .ref()
-                            .child('patients')
-                            .child(account.team as String)
-                            .child(patient.nationalId as String)
-                            .update({keyOfDataBase: controller.text});
-                      else {
-                        await FirebaseDatabase.instance
-                            .ref()
-                            .child('patients')
-                            .child(patient.team as String)
-                            .child(controller.text)
-                            .update({
-                          'team': patient.team,
-                          'volanteerId': patient.volId,
-                          'volanteerName': patient.volName,
-                          'patientName': patient.name,
-                          'nationaId': controller.text,
-                          'docId': patient.docId,
-                          'docName': patient.doctor,
-                          'illnessType': patient.illnessType,
-                          'illness': patient.illness,
-                          'costs': Map.fromIterable(
-                            patient.costs,
-                            key: (e) => e['id'],
-                            value: (e) => {
-                              'التكليف': e['التكليف'],
-                              'القيمة': e['القيمة']
-                            },
-                          ),
-                          'adress': patient.address,
-                          'phone': patient.phone,
-                          'source': patient.source,
-                          'latests': Map.fromIterable(
-                            patient.latests,
-                            key: (e) {
-                              return e['id'];
-                            },
-                            value: (e) {
-                              if (e['date'] == null)
-                                e['date'] = DateTime.now().toIso8601String();
-                              return {
-                                'date': e['date'],
-                                'title': e['title'],
-                              };
-                            },
-                          ),
-                          'state': patient.state,
-                          'date': patient.date,
-                        });
-                        await FirebaseDatabase.instance
-                            .ref()
-                            .child('patients')
-                            .child(patient.team as String)
-                            .child(patient.nationalId as String)
-                            .set(null);
-                      }
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pop();
-                      account.setCurrent(account.current);
-                    },
-                    child: Text('save'));
-              }),
-            ],
-          ),
-        );
-}
-
-class DropDownDialog extends Dialog {
-  DropDownDialog(
-      {required String typeChanges,
-      required Widget dropDownButton,
-      required String changes})
-      : super(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(typeChanges),
-              dropDownButton,
-              Builder(builder: (context) {
-                final account = Provider.of<Account>(context, listen: false);
-                final patient = Provider.of<Patient>(context, listen: false);
-                return TextButton(
-                    onPressed: () async {
-                      await FirebaseDatabase.instance
-                          .ref()
-                          .child('patients')
-                          .child(account.team as String)
-                          .child(patient.nationalId as String)
-                          .update((changes != 'docName')
-                              ? {
-                                  changes: (changes == 'state')
-                                      ? patient.state
-                                      : patient.illnessType,
-                                }
-                              : {
-                                  'docName': patient.doctor,
-                                  'docId': patient.docId,
-                                });
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pop();
-
-                      account.setCurrent(account.current);
-                    },
-                    child: Text('save'));
-              }),
-            ],
-          ),
-        );
 }
